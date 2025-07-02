cecce137b42c541704c6b973f0e6bfe0cafcab43
Victor DIAS CARVALHO <38004390+vectorwade@users.noreply.github.com>
2025-07-02 18:02:51 -0300

    Update ServidorCalculadora.java

--- src/servidor/ServidorCalculadora.java
+++ src/servidor/ServidorCalculadora.java
@@ -5,10 +5,7 @@ import java.rmi.registry.Registry;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-/**
- * Classe principal do servidor RMI
- * Responsável por inicializar o registro RMI e disponibilizar o serviço de calculadora
- */
+
 public class ServidorCalculadora {
     
     private static final int PORTA_RMI = 1099;
@@ -19,14 +16,14 @@ public class ServidorCalculadora {
         try {
             System.out.println("[" + LocalDateTime.now().format(formatter) + "] Iniciando servidor de calculadora RMI...");
             
-            // Criar uma instância da implementação da calculadora
+            
             CalculadoraImpl calculadora = new CalculadoraImpl();
             
-            // Criar o registro RMI na porta padrão
+          
             Registry registry = LocateRegistry.createRegistry(PORTA_RMI);
             System.out.println("[" + LocalDateTime.now().format(formatter) + "] Registro RMI criado na porta " + PORTA_RMI);
             
-            // Registrar o serviço no registro RMI
+            
             registry.rebind(NOME_SERVICO, calculadora);
             System.out.println("[" + LocalDateTime.now().format(formatter) + "] Serviço '" + NOME_SERVICO + "' registrado no RMI");
             
@@ -34,7 +31,7 @@ public class ServidorCalculadora {
             System.out.println("[" + LocalDateTime.now().format(formatter) + "] Para conectar use: rmi://localhost:" + PORTA_RMI + "/" + NOME_SERVICO);
             System.out.println("[" + LocalDateTime.now().format(formatter) + "] Pressione Ctrl+C para parar o servidor");
             
-            // Manter o servidor em execução
+           
             while (true) {
                 Thread.sleep(1000);
             }
